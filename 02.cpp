d0b3630f8e044f4c039797801a138f5ee613f1c8
HugDi <113516550+GugDi@users.noreply.github.com>
2023-07-02 21:42:06 +0400

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class TPen {
+private:
+	int FColor = 0;
+public:
+	TPen() {}
+	int GetColor() {
+		return FColor;
+	}
+	void SetColor(int color) {
+		FColor = color;
+	}
+};
+int main()
+{
+	TPen pencil;
+	cout << pencil.GetColor() << endl;
+	pencil.SetColor(1);
+	cout << pencil.GetColor() << endl;
+	return 0;
+}
